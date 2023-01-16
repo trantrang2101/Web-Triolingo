0ac4a35ca51eae45f2d0c572460763b84920ba1d
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-01-16 23:00:22 +0700

    add logger using log4net

---
+++ Web-Triolingo/Logger/Log4NetCore.cs
@@ -0,0 +1,78 @@
+﻿using log4net;
+
+namespace Web_Triolingo.Logger
+{
+    public class Log4NetCore : ILogger
+    {
+        private static ILog logger = LogManager.GetLogger(typeof(Log4NetCore));
+
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            try
+            {
+                if (state is Array)
+                {
+                    var fileds = state as Array;
+                    foreach (var filed in fileds)
+                    {
+                        if (filed is KeyValuePair<string, string>)
+                        {
+                            var prop = (KeyValuePair<string, string>)filed;
+                            LogicalThreadContext.Properties[prop.Key] = prop.Value;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                logger.Error(string.Format("BeginScope error {0}", e.Message));
+            }
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            string message = null;
+            if (null != formatter)
+            {
+                message = formatter(state, exception);
+            }
+            if (!string.IsNullOrEmpty(message) || exception != null)
+            {
+                //if (!message.Contains("Cloud.Sdk") && !message.Contains("Api.Controllers.SettingsController"))
+                //{
+                //    return;
+                //}
+                switch (logLevel)
+                {
+                    case LogLevel.Critical:
+                        logger.Error(message);
+                        break;
+                    case LogLevel.Debug:
+                    case LogLevel.Trace:
+                        logger.Debug(message);
+                        break;
+                    case LogLevel.Error:
+                        logger.Error(message);
+                        break;
+                    case LogLevel.Information:
+                        logger.Info(message);
+                        break;
+                    case LogLevel.Warning:
+                        logger.Warn(message);
+                        break;
+                    default:
+                        logger.Warn($"Encountered unknown log level {logLevel}, writing out as Info.");
+                        logger.Info(message);
+                        break;
+                }
+            }
+        }
+
+    }
+}
---
+++ Web-Triolingo/Logger/Log4NetManager.cs
@@ -0,0 +1,31 @@
+﻿using log4net.Config;
+using System.Reflection;
+
+namespace Web_Triolingo.Logger
+{
+    public class Log4NetManager : ILoggerProvider
+    {
+        public Log4NetManager(string filename = "log4net.config")
+        {
+            var logRepository = log4net.LogManager.GetRepository(Assembly.GetEntryAssembly());
+            var file = new FileInfo(filename);
+            XmlConfigurator.Configure(logRepository, file);
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new Log4NetCore();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        public virtual void Dispose(bool disposing)
+        {
+            //cleanup
+        }
+
+    }
+}
