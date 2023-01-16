2a5516386d8a39baaf1b9eecd1e54e37a7e607de
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-01-16 22:42:56 +0700

    delete startup and using program

--- Web-Triolingo/Program.cs
+++ Web-Triolingo/Program.cs
@@ -1,7 +1,22 @@
-using Web_Triolingo;
 
 var builder = WebApplication.CreateBuilder(args);
-var startup = new Startup(builder.Configuration);
-startup.ConfigureServices(builder.Services); // calling ConfigureServices method
+
+// Add services to the container.
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 var app = builder.Build();
-startup.Configure(app, builder.Environment); // calling Configure method
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
