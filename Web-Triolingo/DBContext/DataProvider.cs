a4dcce28af7c7d24f90349d741bc004ef5964e49
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-01 23:06:29 +0700

    Add DataProvider, demo  load data from DB

---
+++ Web-Triolingo/DBContext/DataProvider.cs
@@ -0,0 +1,28 @@
+﻿using Web_Triolingo.Models;
+
+namespace Web_Triolingo.DBContext
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public TriolingoDBContext DB { get; set; }
+        public DataProvider()
+        {
+            DB = new TriolingoDBContext();
+        }
+    }
+}
