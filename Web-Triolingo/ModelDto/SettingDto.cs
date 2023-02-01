e6cff10674eba359c214a9449fb3c4aa73703156
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-02 00:03:35 +0700

    add DTO and Service

---
+++ Web-Triolingo/ModelDto/SettingDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Web_Triolingo.ModelDto
+{
+    public class SettingDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Status { get; set; }
+        public string? Note { get; set; }
+        public string? Value { get; set; }
+        public string? ParentName { get; set; }
+    }
+}
