d8407d2e88aa322014c3c5516463268e41de1d15
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-12 10:11:58 +0700

    add new setting, edit setting

--- Web-Triolingo/Interface/Settings/ISettingService.cs
+++ Web-Triolingo/Interface/Settings/ISettingService.cs
@@ -9,7 +9,7 @@ namespace Web_Triolingo.Interface.Settings
         Task<bool> DeactiveSetting(int? settingId);
         Task<bool> ActiveSetting(int? settingId);
         Task<SettingDto> GetSettingById(int? id);
-        Task<List<SettingDto>> GetSettingByParentId(int settingId);
+        Task<List<SettingDto>> GetSettingByParentId(int? settingId);
         Task<List<SettingDto>> GetSettingsNoParentId();
         Task<bool> AddNewSetting(SettingDto setting);
     }
