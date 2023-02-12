ad32fb3437245a1475d0b40e14a8b5e953779960
tienanhcapuchino <tienanh27yp1bn@gmail.com>
2023-02-12 15:23:43 +0700

    add paging model

---
+++ Web-Triolingo/Common/PagingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Web_Triolingo.Common
+{
+    public class PagingModel
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public string FilterValue { get; set; }
+    }
+}
